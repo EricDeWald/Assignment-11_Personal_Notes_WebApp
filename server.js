e692f90a2cd16f72fc4123a654f0a5eec7273496
EricDeWald <eric.l.dewald@gmail.com>
2022-01-24 20:30:02 -0600

    note id update
    I needed to add a random number for the id for the notes object.

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
+const { randomUUID } = require('crypto');
 const { application } = require('express');
 const express = require('express');
 const fs = require("fs")
 const path = require('path');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
-
 // GET request for saved notes
 app.get('/api/notes', (req, res) => 
   res.sendFile(path.join(__dirname,'./db/db.json')))
-
 // POST request to save notes
 app.post('/api/notes', async (req, res) => {
- const notes = await JSON.parse(fs.readFileSync('./db/db.json'))
- notes.push(req.body)
- fs.writeFileSync('./db/db.json', JSON.stringify(notes))
- res.redirect('/notes')
+ const {title,text} = req.body
+ if (req.body){
+   const notesData = {title,text, id:randomUUID()}
+   const notes = await JSON.parse(fs.readFileSync('./db/db.json'))
+   notes.push(notesData)
+   fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+   res.redirect('/notes')
+  }
 })
+
+// app.delete('/api/notes/:id')
