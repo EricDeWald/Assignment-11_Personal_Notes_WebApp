6bf79c75e0328e5f4dfe101b745867283a39136f
EricDeWald <eric.l.dewald@gmail.com>
2022-02-07 17:22:28 -0600

    fixed uuid and refactored get route

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const { randomUUID } = require('crypto');
 const { application } = require('express');
 const express = require('express');
 const fs = require("fs")
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const { v4: uuidv4 } = require('uuid');
+const {readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils')
+// readFromFile, writeToFile, readAndAppend 
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,20 +18,27 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 // GET request for saved notes
-app.get('/api/notes', (req, res) => 
-  res.sendFile(path.join(__dirname,'./db/db.json')))
+app.get('/api/notes', async(req, res) =>{ 
+  const saveRequest = await readFromFile('./db/db.json')
+  res.json(JSON.parse(saveRequest))
+})
 // POST request to save notes
-app.post('/api/notes', async (req, res) => {
- const {title,text} = req.body
- if (req.body){
-   const notesData = {title,text, id:randomUUID()}
-   const notes = await JSON.parse(fs.readFileSync('./db/db.json'))
-   notes.push(notesData)
-   fs.writeFileSync('./db/db.json', JSON.stringify(notes))
-   res.redirect('/notes')
-  }
+app.post('/api/notes', async (req, res) => {const {title,text} = req.body
+try {if (req.body){
+  let id = uuidv4();
+  const notesData = {title,text, id}
+  console.log(notesData)
+  const notes = await JSON.parse(fs.readFileSync('./db/db.json'))
+  notes.push(notesData)
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+  res.redirect('/notes')
+}
+
+} catch (error) {
+  console.log(error)
+} 
+
 })
 
-// app.delete('/api/notes/:id')
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
